c2ac08bd69ad30444745ce4c0110786fbf3d5c29
Sagnik Mandal <sonalisagnik123@gmail.com>
2020-06-15 00:08:30 +0530

    Update search.js

--- search.js
+++ search.js
@@ -40,7 +40,7 @@ const displayResult = result => {
   resultBlockElement.classList.remove('invisible');
 
   // Setting the color of the result text depending on the response label
-  const label = result.url;
+  const label = result.response.items.url;
   const resultElement = document.getElementById('result');
   resultElement.setAttribute('class', label);
   let resultText = label;
