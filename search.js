c7cc6e9927b3ec2de7b6609b95fef8d445a5000f
Sagnik Mandal <sonalisagnik123@gmail.com>
2020-06-18 18:50:26 +0530

    Update search.js

--- search.js
+++ search.js
@@ -1,50 +1,37 @@
 // Text-Processing API Url
-const API_URL = 'https://api.vk.com/method/docs.search?count=1&access_token=f02dc4441e373901b94cd09b9992449f04b6745380f0407c22ebcf1efafdb0c8d29ee8945f407d70cea78&v=5.110&q=';
+const API_URL = 'https://api.vk.com/method/docs.search?access_token=aa38a866def346043b87f00936a2a206625c4b876a7bcb1f889cc6e23b19da05a491ea70d27dd23d37e39&v=5.110&q=';
 
 // Button click handler
 const onAnalyzeButtonClick = () => {
   // Getting a textarea element with a comment
   const commentElement = document.getElementById('comment');
   // Getting comment text
-  const commentText = commentElement.value.trim();
+  const commentText = commentElement.value;
+const resultElement = document.getElementById('urls');
 
   // Handle empty comment
   if (!commentText) {
           return handleEmptyComment();
   }
   // Calling the API and passing the result with the displayResult as a callback function
-  return analyzeComment(commentText, displayResult);
+  return analyzeComment(commentText,resultElement);
 };
-
-const analyzeComment = (comment, callback) => {
-  const Get_API_URL = API_URL + comment
-  // GET request to server
-  axios.get(API_URL)
-          .then(response => {
-            const data = response.data.items[0].url;
-            // Calling a callback function with data from the server
-            callback(data)
-          })
-          .catch(error => console.error(error))
-};
-
-const handleEmptyComment = () => {
-  const resultBlockElement = document.getElementById('main-result-block');
-  resultBlockElement.classList.add('invisible');
-  return alert('Your comment is empty =(');
-};
-
-const displayResult = result => {
-  // Remove invisible class for main-result-block
-  const resultBlockElement = document.getElementById('main-result-block');
-  resultBlockElement.classList.remove('invisible');
-
-  // Setting the color of the result text depending on the response label
-  const label = result.url;
-  const resultElement = document.getElementById('result');
-  resultElement.setAttribute('class', label);
-  let resultText = label;
-
-  // Setting the result text
-  resultElement.textContent = resultText;
-};
+const analyzeComment = (comment, ul) => {
+const final_URL = API_URL + comment
+  fetch(final_URL)
+  .then((resp) => resp.json())
+  .then(function(data) {
+    let books = data.response.items;
+    return books.map(function(book) {
+      let li = createNode('li'),
+          a = createNode('a');
+      
+      a.innerHTML = `${author.name.first} ${author.name.last}`;
+      append(li, a);
+      append(ul, li);
+    })
+  })
+  .catch(function(error) {
+    console.log(error);
+  });   
+}
